e37634e1abfd0e1f91f0bd218d9429c364138095
Ze Dang <4649tz@gmail.com>
2020-08-12 23:00:58 +0800

    [zedang] feat: finish ex-4

--- src/exercise-4/components/App.js
+++ src/exercise-4/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
-import {Route} from "react-router";
+import {Route, Switch} from "react-router";
 import Home from "./Home";
 import About from "./About";
 import User from "./User";
@@ -9,12 +9,14 @@ import NotMatch from "./NotMatch";
 class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div className='app'>
         <Router>
-            <Route component={NotMatch} />
-            <Route path='/' component={Home} />
-            <Route path='/:user' component={User} />
-            <Route path='/about' component={About} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/:user(\d+)' component={User} />
+            <Route exact path='/about' component={About} />
+            <Route path='*' component={NotMatch} />
+          </Switch>
         </Router>
       </div>
     );
